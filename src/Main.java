0d25a1e400e466427247dc60b7c049073c5c0e76
yahyoHakimov <hakimovyahyo7@gmail.com>
2023-03-07 22:56:31 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import methods.SumOf1dArray;
+
+public class Main {
+    public static void main(String[] args) {
+        SumOf1dArray array = new SumOf1dArray();
+        int[] sums = {1,2,3,4,5};
+        array.sumOfArray(sums);
+
+    }
+}
